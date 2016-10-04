2173f1e10caf83974690acd6da969f593e78c75c
Amitai Schlair <schmonz-web-git@schmonz.com>
2016-10-04 11:35:00 -0400

    Inline isnt_brie_or_backstage_passes().

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -29,11 +29,6 @@ bool is_passes(Item item)
     return (0 == strcmp(item.name, "Backstage passes to a TAFKAL80ETC concert"));
 }
 
-bool isnt_brie_or_backstage_passes(Item item)
-{
-    return (!is_brie(item) && !is_passes(item));
-}
-
 bool is_sulfuras(Item item)
 {
     return (0 == strcmp(item.name, "Sulfuras, Hand of Ragnaros"));
@@ -46,7 +41,7 @@ update_quality(Item items[], int size)
     
     for (i = 0; i < size; i++)
     {
-        if (isnt_brie_or_backstage_passes(items[i]))
+        if (!is_brie(items[i]) && !is_passes(items[i]))
         {
             if (items[i].quality > 0 && !is_sulfuras(items[i]))
             {
