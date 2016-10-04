d1931c5a708dec3cc57160652ec292bcadf81041
Amitai Schlair <schmonz-web-git@schmonz.com>
2016-10-04 10:39:54 -0400

    Extract function: is_brie_or_backstage_passes().

--- C/GildedRose.c
+++ C/GildedRose.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <string.h>
 #include "GildedRose.h"
 #include <stdio.h>
@@ -18,14 +19,20 @@ print_item(char* buffer, Item* item)
     sprintf(buffer, "%s, %d, %d", item->name, item->sellIn, item->quality);
 }
 
-void 
+bool is_brie_or_backstage_passes(Item item)
+{
+    return (strcmp(item.name, "Aged Brie")
+            && strcmp(item.name, "Backstage passes to a TAFKAL80ETC concert"));
+}
+
+void
 update_quality(Item items[], int size) 
 {
     int i;
     
     for (i = 0; i < size; i++)
     {
-        if (strcmp(items[i].name, "Aged Brie") && strcmp(items[i].name, "Backstage passes to a TAFKAL80ETC concert"))
+        if (is_brie_or_backstage_passes(items[i]))
         {
             if (items[i].quality > 0)
             {
